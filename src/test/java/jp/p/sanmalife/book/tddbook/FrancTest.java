8a7ba1a03bc1b1a4c009a0ce18c6520531275f37
Takeshi KOZAI <sanmalife@gmail.com>
2012-09-11 02:33:46 +0900

    Francのテストコード追加
    Franc#equalsのテストを作成

--- src/test/java/jp/p/sanmalife/book/tddbook/FrancTest.java
+++ src/test/java/jp/p/sanmalife/book/tddbook/FrancTest.java
@@ -1,6 +1,8 @@
 package jp.p.sanmalife.book.tddbook;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -11,5 +13,11 @@ public class FrancTest {
         Franc five = new Franc(5);
         assertEquals(new Franc(10), five.times(2));
         assertEquals(new Franc(15), five.times(3));
+    }
+
+    @Test
+    public void testEquality() throws Exception {
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
     }
 }
