6a0809d408b88082334faf1c6a719baec40ed1eb
Takeshi KOZAI <sanmalife@gmail.com>
2012-09-12 00:54:19 +0900

    リファクタリング
    timesでファクトリを利用するように変更

--- src/main/java/jp/p/sanmalife/book/tddbook/Dollar.java
+++ src/main/java/jp/p/sanmalife/book/tddbook/Dollar.java
@@ -9,7 +9,7 @@ public class Dollar extends Money {
 
     @Override
     public Money times(int multiplier) {
-        return new Dollar(amount * multiplier, null);
+        return Money.dollar(amount * multiplier);
     }
 
 }
