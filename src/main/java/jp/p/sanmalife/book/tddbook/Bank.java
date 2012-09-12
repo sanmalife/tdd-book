d68c672ec1f48de05bd7c02f022f13cd7722cb78
Takeshi KOZAI <sanmalife@gmail.com>
2012-09-12 22:54:13 +0900

    Bank.reduce(Money)の実装

--- src/main/java/jp/p/sanmalife/book/tddbook/Bank.java
+++ src/main/java/jp/p/sanmalife/book/tddbook/Bank.java
@@ -3,6 +3,10 @@ package jp.p.sanmalife.book.tddbook;
 public class Bank {
 
     public Money reduce(Expression source, String to) {
+        if (source instanceof Money) {
+            return (Money) source;
+        }
+
         Sum sum = (Sum) source;
         return sum.reduce(to);
     }
