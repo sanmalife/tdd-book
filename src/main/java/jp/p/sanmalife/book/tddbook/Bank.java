e0b8958f265ef11a06c8ac15f2f53c26988d8fdd
Takeshi KOZAI <sanmalife@gmail.com>
2012-09-13 02:45:14 +0900

    仮実装(12章復習終了)

--- src/main/java/jp/p/sanmalife/book/tddbook/Bank.java
+++ src/main/java/jp/p/sanmalife/book/tddbook/Bank.java
@@ -3,7 +3,7 @@ package jp.p.sanmalife.book.tddbook;
 public class Bank {
 
     public Money reduce(Expression source, String to) {
-        return null;
+        return Money.dollar(10);
     }
 
 }
