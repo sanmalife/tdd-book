a0da217ede31165c102eca4ce7dd220e2ef98104
Takeshi KOZAI <sanmalife@gmail.com>
2012-09-13 02:12:20 +0900

    テストケースにコメントを追加

--- src/test/java/jp/p/sanmalife/book/tddbook/MoneyTest.java
+++ src/test/java/jp/p/sanmalife/book/tddbook/MoneyTest.java
@@ -8,25 +8,42 @@ import org.junit.Test;
 
 public class MoneyTest {
 
+    /**
+     * 同値性の比較
+     */
     @Test
-    public void testEquality() throws Exception {
+    public void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
 
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
+    /**
+     * 同一通貨同士の掛け算
+     */
     @Test
-    public void testMultiplication() throws Exception {
+    public void testMultiplication() {
         Money five = Money.dollar(5);
         assertEquals(Money.dollar(10), five.times(2));
         assertEquals(Money.dollar(15), five.times(3));
     }
 
+    /**
+     * 通貨種別の取得
+     */
     @Test
-    public void testCurrency() throws Exception {
+    public void testCurrency() {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
+    }
+
+    /**
+     * 同一通貨同士の加算
+     */
+    @Test
+    public void testSimpleAddition() {
+
     }
 
 }
