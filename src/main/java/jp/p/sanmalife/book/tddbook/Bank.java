b73932e569c5ebff9cf9e6c13d21d60f9a7f87df
Takeshi KOZAI <sanmalife@gmail.com>
2012-09-12 22:17:17 +0900

    仮実装

--- src/main/java/jp/p/sanmalife/book/tddbook/Bank.java
+++ src/main/java/jp/p/sanmalife/book/tddbook/Bank.java
@@ -3,7 +3,7 @@ package jp.p.sanmalife.book.tddbook;
 public class Bank {
 
     public Money reduce(Expression sum, String string) {
-        return null;
+        return Money.dollar(10);
     }
 
 }
