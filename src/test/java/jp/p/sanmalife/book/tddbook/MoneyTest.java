c648790ffa2503a68de37928000946421c83b076
Takeshi KOZAI <sanmalife@gmail.com>
2012-09-14 00:39:49 +0900

    Bank#rateを実装

--- src/test/java/jp/p/sanmalife/book/tddbook/MoneyTest.java
+++ src/test/java/jp/p/sanmalife/book/tddbook/MoneyTest.java
@@ -95,4 +95,13 @@ public class MoneyTest {
         Money result = bank.reduce(Money.franc(2), "USD");
         assertEquals(Money.dollar(1), result);
     }
+
+    /**
+     * 同一通貨同士のレートは1
+     */
+    @Test
+    public void testIdentityRate() {
+        assertEquals(1, new Bank().rate("USD", "USD"));
+    }
+
 }
