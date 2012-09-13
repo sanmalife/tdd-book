30bd411870908ddf3bab741d24267878f5053fcd
Takeshi KOZAI <sanmalife@gmail.com>
2012-09-14 00:45:35 +0900

    Pairクラスをinnerクラス化

--- src/main/java/jp/p/sanmalife/book/tddbook/Bank.java
+++ src/main/java/jp/p/sanmalife/book/tddbook/Bank.java
@@ -21,4 +21,25 @@ public class Bank {
 
         return rates.get(new Pair(from, to));
     }
+
+    private class Pair {
+        private String from;
+        private String to;
+
+        public Pair(String from, String to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            Pair pair = (Pair) obj;
+            return from.equals(pair.from) && to.equals(pair.to);
+        }
+
+        @Override
+        public int hashCode() {
+            return 0;
+        }
+    }
 }
