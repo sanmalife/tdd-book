b82025e3b982784b4e437ab40623411357d16a46
Takeshi KOZAI <sanmalife@gmail.com>
2012-09-14 00:35:16 +0900

    Bankが管理する通貨ペアとしてPairクラスを導入
    まだ動かない

--- src/main/java/jp/p/sanmalife/book/tddbook/Bank.java
+++ src/main/java/jp/p/sanmalife/book/tddbook/Bank.java
@@ -1,15 +1,20 @@
 package jp.p.sanmalife.book.tddbook;
 
+import java.util.Hashtable;
+
 public class Bank {
+
+    private Hashtable<Pair, Integer> rates = new Hashtable<Pair, Integer>();
 
     public Money reduce(Expression source, String to) {
         return source.reduce(this, to);
     }
 
-    public void addRate(String string, String string2, int i) {
+    public void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), rate);
     }
 
     int rate(String from, String to) {
-        return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
+        return rates.get(new Pair(from, to));
     }
 }
---
+++ src/main/java/jp/p/sanmalife/book/tddbook/Pair.java
@@ -0,0 +1,22 @@
+package jp.p.sanmalife.book.tddbook;
+
+public class Pair {
+    private String from;
+    private String to;
+
+    public Pair(String from, String to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Pair pair = (Pair) obj;
+        return from.equals(pair.from) && to.equals(pair.to);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+}
