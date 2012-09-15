dbf8c348ff3235407f19e1bfd78e4e48125ec9bf
Takeshi KOZAI <sanmalife@gmail.com>
2012-09-15 21:39:47 +0900

    Sum#plusのテストケースを作成

--- src/test/java/jp/p/sanmalife/book/tddbook/MoneyTest.java
+++ src/test/java/jp/p/sanmalife/book/tddbook/MoneyTest.java
@@ -64,6 +64,20 @@ public class MoneyTest {
     }
 
     /**
+     * Sumのplus
+     */
+    @Test
+    public void testSumPlusMoney() {
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Expression sum = new Sum(fiveBucks, tenFrancs).plus(fiveBucks);
+        Money result = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(15), result);
+    }
+
+    /**
      * 加算の結果はSumを表現するExpressionである
      */
     @Test
