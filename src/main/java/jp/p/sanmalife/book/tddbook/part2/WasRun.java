4472fb11ca12df138e89ac84e9d188dfa6728b20
Takeshi KOZAI <sanmalife@gmail.com>
2012-09-16 21:26:11 +0900

    WasRunの最初のテストを作成
    メソッド呼び出し結果を取得する機能

---
+++ src/main/java/jp/p/sanmalife/book/tddbook/part2/WasRun.java
@@ -0,0 +1,14 @@
+package jp.p.sanmalife.book.tddbook.part2;
+
+public class WasRun {
+
+    public boolean wasRun;
+
+    public WasRun(String string) {
+        wasRun = false;
+    }
+
+    public void testMethod() {
+    }
+
+}
---
+++ src/test/java/jp/p/sanmalife/book/tddbook/part2/WasRunTest.java
@@ -0,0 +1,17 @@
+package jp.p.sanmalife.book.tddbook.part2;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class WasRunTest {
+
+    @Test
+    public void testTestMethod() {
+        WasRun test = new WasRun("testMethod");
+        assertEquals(false, test.wasRun);
+        test.testMethod();
+        assertEquals(true, test.wasRun);
+    }
+
+}
