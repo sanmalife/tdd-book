884aede72fef12fee165ac399e21fbf65492f426
Takeshi KOZAI <sanmalife@gmail.com>
2012-09-17 02:41:16 +0900

    リファクタリング
    重複をsetUpに移動

--- src/test/java/jp/p/sanmalife/book/tddbook/part2/TestCaseTest.java
+++ src/test/java/jp/p/sanmalife/book/tddbook/part2/TestCaseTest.java
@@ -10,36 +10,36 @@ import static org.junit.Assert.assertEquals;
  */
 public class TestCaseTest extends TestCase {
 
+    private TestResult result;
+
     public TestCaseTest(String name) {
         super(name);
     }
 
     @Override
     public void setUp() {
+        result = new TestResult();
     }
 
     public void testTemplateMethod() {
         WasRun test = new WasRun("testMethod");
-        test.run(new TestResult());
+        test.run(result);
         assertEquals("setUp testMethod tearDown ", test.log);
     }
 
     public void testResult() {
         WasRun test = new WasRun("testMethod");
-        TestResult result = new TestResult();
         test.run(result);
         assertEquals("1 run, 0 failed", result.summary());
     }
 
     public void testFailedResult() {
         WasRun test = new WasRun("testBrokenMethod");
-        TestResult result = new TestResult();
         test.run(result);
         assertEquals("1 run, 1 failed", result.summary());
     }
 
     public void testFailedResultFormatting() {
-        TestResult result = new TestResult();
         result.testStarted();
         result.testFailed();
         assertEquals("1 run, 1 failed", result.summary());
@@ -49,7 +49,6 @@ public class TestCaseTest extends TestCase {
         TestSuite suite = new TestSuite();
         suite.add(new WasRun("testMethod"));
         suite.add(new WasRun("testBrokenMethod"));
-        TestResult result = new TestResult();
         suite.run(result);
         assertEquals("2 run, 1 failed", result.summary());
     }
