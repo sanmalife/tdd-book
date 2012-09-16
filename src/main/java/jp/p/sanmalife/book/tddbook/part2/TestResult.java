ae1592e111e9d44ec205baa6cf8e75d7b8241148
Takeshi KOZAI <sanmalife@gmail.com>
2012-09-17 01:48:12 +0900

    テストが失敗するときのフォーマットが正しくなるよう実装を追加

--- src/main/java/jp/p/sanmalife/book/tddbook/part2/TestResult.java
+++ src/main/java/jp/p/sanmalife/book/tddbook/part2/TestResult.java
@@ -2,15 +2,17 @@ package jp.p.sanmalife.book.tddbook.part2;
 
 public class TestResult {
     int runCount = 0;
+    int errorCount = 0;
 
     public void testStarted() {
         runCount++;
     }
 
     public void testFailed() {
+        errorCount++;
     }
 
     public String summary() {
-        return String.format("%d run, 0 failed", runCount);
+        return String.format("%d run, %d failed", runCount, errorCount);
     }
 }
