0d3fa9dbdb3d79c5bb4e43d875c7239ad43265c9
Takeshi KOZAI <sanmalife@gmail.com>
2012-09-16 21:57:45 +0900

    動的にメソッドを呼び出すようにした

--- src/main/java/jp/p/sanmalife/book/tddbook/part2/WasRun.java
+++ src/main/java/jp/p/sanmalife/book/tddbook/part2/WasRun.java
@@ -1,10 +1,14 @@
 package jp.p.sanmalife.book.tddbook.part2;
 
+import java.lang.reflect.Method;
+
 public class WasRun {
 
     public boolean wasRun;
+    private String name;
 
-    public WasRun(String string) {
+    public WasRun(String name) {
+        this.name = name;
         wasRun = false;
     }
 
@@ -13,7 +17,12 @@ public class WasRun {
     }
 
     public void run() {
-        testMethod();
+        try {
+            Class clzz = this.getClass();
+            Method method = clzz.getMethod(name, null);
+            method.invoke(this, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
 }
