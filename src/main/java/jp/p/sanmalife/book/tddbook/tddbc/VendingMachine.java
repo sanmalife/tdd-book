dc7c3777928ac93b7ca0a9aac7b1c22517eadb54
Takeshi KOZAI <sanmalife@gmail.com>
2012-09-18 23:26:06 +0900

    リファクタリング
    フィールドの名称変更
    beforeメソッドの抽出

--- src/main/java/jp/p/sanmalife/book/tddbook/tddbc/VendingMachine.java
+++ src/main/java/jp/p/sanmalife/book/tddbook/tddbc/VendingMachine.java
@@ -1,11 +1,26 @@
 package jp.p.sanmalife.book.tddbook.tddbc;
 
+/**
+ * 自動販売機クラス
+ * 
+ * @author kozai.takeshi
+ * 
+ */
 public class VendingMachine {
 
-    public int total;
+    /**
+     * 投入金額の総計
+     */
+    public int totalAmount;
 
+    /**
+     * お金を投入する
+     * 
+     * @param money
+     *            投入する金額
+     */
     public void insert(int money) {
-        total += money;
+        totalAmount += money;
     }
 
 }
--- src/test/java/jp/p/sanmalife/book/tddbook/tddbc/VendingMachineTest.java
+++ src/test/java/jp/p/sanmalife/book/tddbook/tddbc/VendingMachineTest.java
@@ -2,28 +2,33 @@ package jp.p.sanmalife.book.tddbook.tddbc;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class VendingMachineTest {
 
+    private VendingMachine vendingMachine;
+
     @Test
     public void 初期状態の時_投入金額の総計は0円() {
-        VendingMachine vendingMachine = new VendingMachine();
-        assertEquals(0, vendingMachine.total);
+        assertEquals(0, vendingMachine.totalAmount);
+    }
+
+    @Before
+    public void before() {
+        vendingMachine = new VendingMachine();
     }
 
     @Test
     public void お金を1回投入すると投入金額の総計が増加する() {
-        VendingMachine vendingMachine = new VendingMachine();
         vendingMachine.insert(10);
-        assertEquals(10, vendingMachine.total);
+        assertEquals(10, vendingMachine.totalAmount);
     }
 
     @Test
     public void お金を2回投入すると投入金額の総計は2回の投入金額の合計になる() {
-        VendingMachine vendingMachine = new VendingMachine();
         vendingMachine.insert(10);
         vendingMachine.insert(50);
-        assertEquals(60, vendingMachine.total);
+        assertEquals(60, vendingMachine.totalAmount);
     }
 }
